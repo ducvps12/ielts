0098bfc1e3ae2709dd1cba4cfcd2ea6937151bdc
ducvps12 <mtienduc@gmail.com>
2026-08-05 13:18:08 +0700

    feat(api): register Video Lab capabilities endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from "@nestjs/common";
 
 import { PaymentCapabilitiesController } from "./commerce/payment-capabilities.controller.js";
 import { HealthController } from "./health/health.controller.js";
+import { VideoLearningCapabilitiesController } from "./video-learning/video-learning-capabilities.controller.js";
 
 @Module({
-  controllers: [HealthController, PaymentCapabilitiesController],
+  controllers: [
+    HealthController,
+    PaymentCapabilitiesController,
+    VideoLearningCapabilitiesController,
+  ],
 })
 export class AppModule {}
