c8af5c697eefe7b079f2013d47a5ec2843ec9953
ducvps12 <mtienduc@gmail.com>
2026-08-05 12:48:52 +0700

    feat(api): register commerce capabilities endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 
+import { PaymentCapabilitiesController } from "./commerce/payment-capabilities.controller.js";
 import { HealthController } from "./health/health.controller.js";
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, PaymentCapabilitiesController],
 })
 export class AppModule {}
