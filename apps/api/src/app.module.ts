db1c4e2e3a7530a7a4be61355a03fef5999aba50
ducvps12 <mtienduc@gmail.com>
2026-08-05 16:37:38 +0700

    feat(api): compose auth and goal modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 
+import { AuthModule } from "./auth/auth.module.js";
 import { PaymentCapabilitiesController } from "./commerce/payment-capabilities.controller.js";
+import { GoalsModule } from "./goals/goals.module.js";
 import { HealthController } from "./health/health.controller.js";
 import { VideoLearningCapabilitiesController } from "./video-learning/video-learning-capabilities.controller.js";
 
 @Module({
+  imports: [AuthModule, GoalsModule],
   controllers: [
     HealthController,
     PaymentCapabilitiesController,
