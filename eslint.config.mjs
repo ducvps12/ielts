4397d3f661a3b409c4bf9e558393447d5ccced63
ducvps12 <mtienduc@gmail.com>
2026-08-05 12:07:43 +0700

    chore(quality): add shared eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+  globalIgnores([
+    "**/.next/**",
+    "**/dist/**",
+    "**/coverage/**",
+    "**/node_modules/**",
+    "playwright-report/**",
+    "test-results/**",
+  ]),
+]);
